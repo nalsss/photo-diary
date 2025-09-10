61d993ca1c84f95e19e54fdc06a54a4a2545b17a
Nelson <nels.leung@gmail.com>
2025-09-09 20:46:13 -0400

    Update ping.js

--- functions/api/ping.js
+++ functions/api/ping.js
@@ -1,4 +1,8 @@
-export const onRequestGet = () =>
-  new Response(JSON.stringify({ ok: true, time: new Date().toISOString() }), {
-    headers: { 'content-type': 'application/json' }
-  });
+export const onRequestGet = async ({ env }) => {
+  const hasKV = !!env.PHOTO_FEED && typeof env.PHOTO_FEED.get === 'function';
+  return new Response(JSON.stringify({
+    ok: true,
+    time: new Date().toISOString(),
+    kvBound: hasKV
+  }), { headers: { 'content-type': 'application/json' } });
+};
